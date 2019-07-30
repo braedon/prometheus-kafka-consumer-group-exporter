99d74a6db8991c5ab2e66e9841d982b6d124c979
Braedon Vickers <braedon.vickers@gmail.com>
2019-07-30 20:41:06 +0800

    Target next release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='prometheus-kafka-consumer-group-exporter',
-    version='0.5.1',
+    version='0.5.2.dev1',
     description='Kafka consumer group Prometheus exporter',
     url='https://github.com/Braedon/prometheus-kafka-consumer-group-exporter',
     author='Braedon Vickers',
